68848fb6a7bb95496a4032078caf375264d878c6
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2018-07-30 02:34:27 +0300

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,6 @@
 /*----------СКРИПТ ДЛЯ ПРОЛИСТЫВАНИЯ ЦЕЛОГО ЭКРАНА--------------*/
 if(window.innerWidth > 769){
+/*----------СКРИПТ ДЛЯ ПРОЛИСТЫВАНИЯ ЦЕЛОГО ЭКРАНА--------------*/	
 function slidescreen() { h = $(window).height(); $(".screen").css('height', h); };
 
 $(window).resize(slidescreen);
@@ -29,17 +30,21 @@ function scroll(event) {
     });
   }
 }
+/*----------ИЗМЕНЕНИЕ СКРОЛЛБАРА-----------*/
 $(".container_longList").niceScroll();
 
+/*-----------ПОПАП НА ДЕСКТОПЕ-------*/
 $(".header__menu").click(function(){
-	$(".popup").slideDown();
+	$(".popup_desktop").slideDown();
 })
- 
+
 $(".popup__close").click(function(){
-	$(".popup").slideUp();
+	$(".popup_desktop").slideUp();
 })
 };
 
+
+/*------------ПОПАП НА МОБИЛКЕ-------*/ 
 if(window.innerWidth < 769){
 	$(".header__menu").click(function(){
 		$(".popup_mobile").slideDown();
@@ -58,7 +63,7 @@ $('.container_longList').slick({
 });
 };
 
-
+/*---------СЛАЙДЕР НА 3-ЕМ ЭКРАНЕ-------------*/
 $('.sliderFish').slick({
 	prevArrow: '<svg class="slick-arrow-prev" width="15" height="25" viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.3119 24.7414C13.5048 24.9138 13.746 25 14.0113 25C14.2765 25 14.5177 24.9138 14.7106 24.7414C15.0965 24.3966 15.0965 23.8362 14.7106 23.4914L2.41158 12.5L14.7106 1.50862C15.0965 1.16379 15.0965 0.603448 14.7106 0.258621C14.3248 -0.0862069 13.6977 -0.0862069 13.3119 0.258621L0.289389 11.875C-0.0964634 12.2198 -0.0964634 12.7802 0.289389 13.125L13.3119 24.7414Z" fill="#333333"/></svg>',
 	nextArrow: '<svg class="slick-arrow-next" width="15" height="25" viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.3119 24.7414C13.5048 24.9138 13.746 25 14.0113 25C14.2765 25 14.5177 24.9138 14.7106 24.7414C15.0965 24.3966 15.0965 23.8362 14.7106 23.4914L2.41158 12.5L14.7106 1.50862C15.0965 1.16379 15.0965 0.603448 14.7106 0.258621C14.3248 -0.0862069 13.6977 -0.0862069 13.3119 0.258621L0.289389 11.875C-0.0964634 12.2198 -0.0964634 12.7802 0.289389 13.125L13.3119 24.7414Z" transform="translate(15 25) rotate(180)" fill="#333333"/></svg>',
@@ -74,12 +79,3 @@ $('.sliderFish').slick({
 });
 
  
-
-
-$(".header__menu").click(function(){
-	$(".popup").slideDown();
-})
- 
-$(".popup__close").click(function(){
-	$(".popup").slideUp();
-})
